0ca809385e3f6a4fcdaf788a32c303018271410d
Jubair <jubair.ahmed2838@gmail.com>
2023-09-10 16:16:22 +0600

    flags added

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -8,21 +8,41 @@ const Countries = () => {
     const loadData = async () => {
       const res = await fetch("https://restcountries.com/v3.1/all");
       const data = await res.json();
-     
+
       setData(data);
-      console.log(data)
     };
     loadData();
   }, []);
+  const [goingList, setGoingList] = useState([]);
+  const handleAddToList = (country) => {
+    const updatedList = [...goingList, country];
+    setGoingList(updatedList);
+  };
+
   return (
     <div>
-        <div className="text-center my-4">
+      <div className="text-center my-4">
         <h1 className="text-3xl m-2">Explore the world with React</h1>
         <h2 className="text-2xl font-semibold">Countries: {data.length}</h2>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-3">
-           {data.map(country=><Country country={country} key={country.cca3}></Country>)}
-        </div>
+        <ul className="m-4">
+          {/* display item on the UI */}
+          {goingList.map((country, index)=><div key={index} className="flex items-center gap-4 m-4">
+            <li className="list-disc font-semibold">{country.name.common}</li>
+            <img style={{width:'50px'}} src={country.flags.png} alt="" />
+          </div>)}
+
+        </ul>
+      </div>
+        <h3 className="text-xl font-semibold mx-4">Total: {goingList.length}</h3>
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        {data.map((country) => (
+          <Country
+            country={country}
+            key={country.cca3}
+            handleAddToList={handleAddToList}
+          ></Country>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,20 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const Country = ({country}) => {
-    const {name,population,region, flags} = country
-    const [visited, setVisited] = useState(false)
-    const handleVisited = ()=> setVisited(!visited)
-   
+const Country = ({ country, handleAddToList }) => {
+  const { name, population, region, flags } = country;
+
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => setVisited(!visited);
   return (
-    <div className={`border-2 border-blue-700 text-center p-4 m-4 ${visited && 'bg-blue-400'}`}>
-      <h2 className='text-xl font-semibold'>Name: {name.common}</h2>
-      
-        <img src={flags.png} alt="" />
+    <div
+      className={`border-2 border-blue-700 text-center p-4 m-4 ${
+        visited && "bg-blue-400"
+      }`}
+    >
+      <h2 className="text-xl font-semibold">Name: {name.common}</h2>
+
+      <img src={flags.png} alt="" />
       <p>Population: {population}</p>
       <p>Region: {region}</p>
-      <button onClick={handleVisited} className='btn bg-orange-200'>{visited ? 'Visited' : 'Going'}</button>
+      <div className="flex items-center justify-center">
+        <button onClick={handleVisited} className="btn bg-orange-200 m-4">
+          {visited ? "Visited" : "Going"}
+        </button>
+        <p style={{color: visited? 'white': 'brown'}}>{visited ? 'I have visited '+ name.common : 'I going to '+ name.common }</p>
+      </div>
+      <button
+        onClick={() => handleAddToList(country)}
+        className="btn btn-primary"
+      >
+        Add to list
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Country
+export default Country;
