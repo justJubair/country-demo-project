e245fa1b720247f11a23c232f6178f319b095f44
Jubair <jubair.ahmed2838@gmail.com>
2023-09-10 13:53:56 +0600

    toggle button added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Country = ({country}) => {
     const {name,population,region, flags} = country
+    const [visited, setVisited] = useState(false)
+    const handleVisited = ()=> setVisited(!visited)
+   
   return (
-    <div className='border-2 border-blue-700 text-center p-4 m-4'>
+    <div className={`border-2 border-blue-700 text-center p-4 m-4 ${visited && 'bg-blue-400'}`}>
       <h2 className='text-xl font-semibold'>Name: {name.common}</h2>
       
         <img src={flags.png} alt="" />
       <p>Population: {population}</p>
       <p>Region: {region}</p>
+      <button onClick={handleVisited} className='btn bg-orange-200'>{visited ? 'Visited' : 'Going'}</button>
     </div>
   )
 }
